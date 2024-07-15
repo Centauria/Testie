c1860b37e243c5b9bea263095ec66ae3c4e27b2f
ctr <chentairan@medeli.com>
2024-07-15 18:07:51 +0800

    Python session

--- src/caller/py.rs
+++ src/caller/py.rs
@@ -1,5 +1,7 @@
-use std::process::Command;
-
+use std::process::{Command};
+use expectrl::repl::ReplSession;
+use expectrl::{Regex};
+use crate::util;
 
 pub fn call(filename: &str, args: Option<Vec<&str>>) -> String {
     let mut filename_and_args = vec![filename];
@@ -13,4 +15,18 @@ pub fn call(filename: &str, args: Option<Vec<&str>>) -> String {
         .output()
         .expect("failed");
     String::from_utf8(python_output.stdout).unwrap()
+}
+
+pub fn python_runtime() -> ReplSession {
+    let mut session = expectrl::repl::spawn_python().unwrap();
+    session.execute("import os").unwrap();
+    session.execute(format!("os.chdir(r'{}')", util::get_current_working_dir())).unwrap();
+    session
+}
+pub fn runfile(session: &mut ReplSession, filename: &str) {
+    session.send_line(format!("exec(open('{filename}').read())")).unwrap();
+}
+pub fn read_output(session: &mut ReplSession, regex_string: String) -> String {
+    let found = session.expect(Regex(regex_string)).unwrap();
+    String::from_utf8_lossy(found.get(0).unwrap()).to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@
 
 mod caller;
 mod apps;
+mod util;
 
 use egui;
 use eframe;
 use crate::apps::main::Main;
+use crate::caller::{julia, py};
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -18,11 +20,18 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
 
-    let mut r = caller::julia::call("src_julia/test.jl", None);
+    let mut python_runtime = py::python_runtime();
+    py::runfile(&mut python_runtime, "src_python/test.py");
+    println!("{}", py::read_output(&mut python_runtime, r"\[.*\]".to_owned()));
+    py::runfile(&mut python_runtime, "src_python/test.py");
+    println!("{}", py::read_output(&mut python_runtime, r"\[.*\]".to_owned()));
+    python_runtime.exit().expect("quit failed");
+
+    let mut r = julia::call("src_julia/test.jl", None);
     print!("{}", r);
 
-    r = caller::py::call("src_python/test.py", None);
+    r = py::call("src_python/test.py", None);
     print!("{}", r);
-    
+
     eframe::run_native("Testie", options, Box::new(|_cc| Ok(Box::<Main>::default())))
 }
